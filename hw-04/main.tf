a2a040923fa24d0262b0e8e3cdcd6a11dad3c7ce
ArturP1rozhkov <turistosartur@gmail.com>
2026-05-24 14:50:02 +0000

    hw-04: task 1

--- hw-04/main.tf
+++ hw-04/main.tf
@@ -1,6 +1,7 @@
 resource "yandex_vpc_network" "develop" {
   name = var.vpc_name
 }
+
 resource "yandex_vpc_subnet" "develop" {
   name           = var.vpc_name
   zone           = var.default_zone
@@ -8,3 +9,57 @@ resource "yandex_vpc_subnet" "develop" {
   v4_cidr_blocks = var.default_cidr
 }
 
+locals {
+  ssh_key = file("~/.ssh/id_ed25519.pub")
+}
+
+data "template_file" "cloudinit" {
+  template = file("${path.module}/cloud-init.yml")
+  vars = {
+    ssh_keys = local.ssh_key
+  }
+}
+
+module "marketing_vm" {
+  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  env_name       = "develop"
+  network_id     = yandex_vpc_network.develop.id
+  subnet_zones   = [var.default_zone]
+  subnet_ids     = [yandex_vpc_subnet.develop.id]
+  instance_name  = "marketing"
+  instance_count = 1
+  image_family   = "ubuntu-2004-lts"
+  public_ip      = true
+
+  labels = {
+    owner   = "team-marketing"
+    project = "marketing"
+  }
+
+  metadata = {
+    user-data          = data.template_file.cloudinit.rendered
+    serial-port-enable = 1
+  }
+}
+
+module "analytics_vm" {
+  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  env_name       = "develop"
+  network_id     = yandex_vpc_network.develop.id
+  subnet_zones   = [var.default_zone]
+  subnet_ids     = [yandex_vpc_subnet.develop.id]
+  instance_name  = "analytics"
+  instance_count = 1
+  image_family   = "ubuntu-2004-lts"
+  public_ip      = true
+
+  labels = {
+    owner   = "team-analytics"
+    project = "analytics"
+  }
+
+  metadata = {
+    user-data          = data.template_file.cloudinit.rendered
+    serial-port-enable = 1
+  }
+}
