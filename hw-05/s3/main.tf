777d783342452667545f7bf82e1e8bba6d828c28
ArturP1rozhkov <turistosartur@gmail.com>
2026-05-27 14:57:36 +0000

    hotfix files after scan

--- hw-05/s3/main.tf
+++ hw-05/s3/main.tf
@@ -1,5 +1,5 @@
 module "s3" {
-  source = "github.com/terraform-yc-modules/terraform-yc-s3"
+  source = "github.com/terraform-yc-modules/terraform-yc-s3?ref=e4017d77de83fe105604fa7b012bc809a77c2fa2"
 
   bucket_name   = "netology-course-bucket-kva" # уникальное имя по всему облаку
   force_destroy = false                        # флаг не удалять с объектами
